2771c9cc5770d897f3bc0b48c785ae302002c54e
Kirill Zaitsev <kirik910@gmail.com>
2014-09-16 16:11:51 +0400

    Update sesison

--- session.go
+++ session.go
@@ -10,24 +10,26 @@ const (
 )
 
 type Session struct {
-	Id               int    `json:"id,omitempty"`
-	UserName         string `json:"username,omitempty"`
-	Name             string `json:"name,omitempty"`
-	State            string `json:"state,omitempty"`
+	Id               int    `json:"id"`
+	UserName         string `json:"username"`
+	Name             string `json:"name"`
+	Blocked          bool   `json:"blocked"`
+	State            string `json:"state"`
 	AvatarURL        string `json:"avatar_url",omitempty`
-	IsAdmin          bool   `json:"is_admin",omitempty`
-	Email            string `json:"email",omitempty`
+	IsAdmin          bool   `json:"is_admin"`
+	Bio              string `json:"bio",omitempty`
+	Email            string `json:"email"`
 	ThemeId          int    `json:"theme_id",omitempty`
 	ColorSchemeId    int    `json:"color_scheme_id",omitempty`
 	ExternUid        string `json:"extern_uid",omitempty`
 	Provider         string `json:"provider",omitempty`
-	CanCreateGroup   bool   `json:"can_create_group",omitempty`
-	CanCreateProject bool   `json:"can_create_project",omitempty`
+	CanCreateGroup   bool   `json:"can_create_group"`
+	CanCreateProject bool   `json:"can_create_project"`
 	Skype            string `json:"skype",omitempty`
 	Twitter          string `json:"twitter",omitempty`
 	LinkedIn         string `json:"linkedin",omitempty`
 	WebsiteURL       string `json:"website_url",omitempty`
-	PrivateToken     string `json:"private_token",omitempty`
+	PrivateToken     string `json:"private_token"`
 }
 
 func (g *Gitlab) GetSession(email string, password string) (*Session, error) {
